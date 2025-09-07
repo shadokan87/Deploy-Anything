6d05a4b8c30ab70917cd0cd8a0ecc8dccbca64eb
Eclipse TOURE <motoure@f6r10s13.clusters.42paris.fr>
2025-09-07 15:26:02 +0200

    wipm

--- src/app/components/AuthButton.tsx
+++ src/app/components/AuthButton.tsx
@@ -1,33 +1,31 @@
 // app/components/AuthButton.tsx
 'use client';
 
+import { Button } from "@/components/ui/button";
 import { signIn, signOut, useSession } from "next-auth/react";
 
 export default function AuthButton() {
   const { data: session, status } = useSession();
 
-  if (status === "loading") return <p>Chargement...</p>;
+  // if (status === "loading") return <p>Chargement...</p>;
 
   if (session) {
     return (
       <div>
-        <p>Bienvenue, {session.user?.name}</p>
-        <button
+        <Button
           onClick={() => signOut()}
-          className="bg-red-500 text-white px-4 py-2 rounded"
         >
-          Se déconnecter
-        </button>
+          Logout
+        </Button>
       </div>
     );
   }
 
   return (
-    <button
+    <Button
       onClick={() => signIn("github")}
-      className="bg-blue-500 text-white px-4 py-2 rounded"
     >
-      Se connecter avec GitHub
-    </button>
+      Contiue with github
+    </Button>
   );
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { SessionProvider } from "next-auth/react";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,12 +26,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
+      <head />
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <SessionProvider>
-          {children}
+          <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
         </SessionProvider>
       </body>
     </html>
