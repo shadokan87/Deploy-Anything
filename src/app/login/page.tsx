72fbc6a75453ddd4ca271e94ab4dcf0f9d0daedb
Eclipse TOURE <motoure@f6r10s13.clusters.42paris.fr>
2025-09-07 03:43:02 +0200

    wip auth github

---
+++ src/app/login/page.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+const handleGitHubLogin = () => {
+
+};
+
+export default function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <button onClick={handleGitHubLogin}>
+        Continue with GitHub
+      </button>
+    </div>
+  );
+}
